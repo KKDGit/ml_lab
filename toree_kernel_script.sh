73a2ac57ac1ea86f673fd08cc3504fffbb384e20
Kranthi Kumar D <kranthidr@gmail.com>
2018-09-15 06:42:54 -0400

    rddAndStreaming

---
+++ toree_kernel_script.sh
@@ -0,0 +1,14 @@
+#use Toree version 0.2.0 jar
+
+jupyter toree install --kernel_name='scala_nsc' --log-level=50 --spark_home=/usr/hdp/current/spark2-client 
+--toree_opts='--nosparkcontext' --user --replace
+
+jupyter toree install --kernel_name='0609' --log-level=50 --spark_home=/usr/hdp/current/spark2-client --user --replace --spark_opts="--master yarn --jars file://${HOME}/gitRepos/aas/ch06-lsa/target/ch06-lsa-2.0.0-jar-with-dependencies.jar,file://${HOME}/gitRepos/aas/ch09-risk/target/ch09-risk-2.0.0-jar-with-dependencies.jar"
+
+cp ~/.local/share/jupyter/kernels/spark_scala/lib/toree-assembly-0.2.0-incubating.jar ~/.local/share/jupyter/kernels/0609_scala/lib/toree-assembly-0.2.0-incubating.jar
+
+rm ~/.local/share/jupyter/kernels/0609_scala/lib/*0.1.0*.jar
+
+
+
+jupyter toree install --kernel_name='Spark' --log-level=50 --spark_home=/usr/hdp/current/spark2-client --user --replace --spark_opts="--master yarn"
