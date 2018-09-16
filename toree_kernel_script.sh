0d25133b85c5c4c016d378b07a78a57145611b37
Kranthi Kumar D <kranthidr@gmail.com>
2018-09-16 13:59:20 +0000

    nsc

--- toree_kernel_script.sh
+++ toree_kernel_script.sh
@@ -1,8 +1,7 @@
 #use Toree version 0.2.0 jar
 jupyter toree install --kernel_name='Spark' --log-level=50 --spark_home=/usr/hdp/current/spark2-client --user --replace --spark_opts="--master yarn"
 
-jupyter toree install --kernel_name='scala_nsc' --log-level=50 --spark_home=/usr/hdp/current/spark2-client 
---toree_opts='--nosparkcontext' --user --replace
+jupyter toree install --kernel_name='nsc' --log-level=50 --spark_home=/usr/hdp/current/spark2-client --toree_opts='--nosparkcontext' --user --replace
 
 jupyter toree install --kernel_name='0609' --log-level=50 --spark_home=/usr/hdp/current/spark2-client --user --replace --spark_opts="--master yarn --jars file://${HOME}/gitRepos/aas/ch06-lsa/target/ch06-lsa-2.0.0-jar-with-dependencies.jar,file://${HOME}/gitRepos/aas/ch09-risk/target/ch09-risk-2.0.0-jar-with-dependencies.jar"
 
