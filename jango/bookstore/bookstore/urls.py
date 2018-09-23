e1f00be5eb02f0025e47b1d1e2107968550c4e7b
Kranthi Kumar D <kranthidr@gmail.com>
2018-09-23 09:59:07 +0530

    main index view and template

--- jango/bookstore/bookstore/urls.py
+++ jango/bookstore/bookstore/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 urlpatterns = [
     # Examples:
     # url(r'^$', 'bookstore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index, name='main_index'),
     url(r'^store/', include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
